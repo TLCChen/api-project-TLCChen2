abe18d41b8bcf2a1b6a020bd324f64ce38bf6e1f
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-12-05 13:14:23 -0500

    12-5-23

--- apiproject/js/main.js
+++ apiproject/js/main.js
@@ -1,4 +1,6 @@
-const URL = `https://pokeapi.co/api/v2/pokemon/random`;
+const URL = `https://pokeapi.co/api/v2/pokemon/?limit=1292`;
+// take the url and then redo this whole thing but with the url
+const list = [];
 
 async function getData(URL) {
   try {
@@ -11,15 +13,14 @@ async function getData(URL) {
     // take response from serve and convert it to JSON
     const data = await response.json();
     console.log(data);
-    document.querySelector("h1").textContent = data.name;
-    document.querySelector("img").src = data.sprites.front_default;
-    document.querySelector(".box").insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="item">
-      <img src="${data.sprites.front_default}" alt="">
-    </div>`
-    );
+    data.results.forEach((urls) => list.push(urls.url));
+    console.log(list);
+    // document.querySelector("h1").textContent = data.name;
+    // document.querySelector("img").src = data.sprites.front_default;
+    // document.querySelector(".box").insertAdjacentHTML("beforeend", `hi joe`);
+    // Object.keys(data).forEach((keys) => {
+    //   data[keys];
+    // });
   } catch (error) {
     document.querySelector("h1").textContent = error;
     document.querySelector("h1").textContent =
