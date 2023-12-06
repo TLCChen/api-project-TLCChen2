fe831e7aa6a09a8761ba80923560320b3d18fec6
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-12-06 13:14:40 -0500

    12-6-23:2.0

--- apiproject/js/main.js
+++ apiproject/js/main.js
@@ -1,5 +1,7 @@
+// use template literals
 const URL = `https://pokeapi.co/api/v2/pokemon/?limit=1292`;
 // take the url and then redo this whole thing but with the url
+
 const list = [];
 
 async function getData(URL) {
@@ -13,14 +15,9 @@ async function getData(URL) {
     // take response from serve and convert it to JSON
     const data = await response.json();
     console.log(data);
-    data.results.forEach((urls) => list.push(urls.url));
+    const list = data.results.map((take) => take.url);
     console.log(list);
-    // document.querySelector("h1").textContent = data.name;
-    // document.querySelector("img").src = data.sprites.front_default;
-    // document.querySelector(".box").insertAdjacentHTML("beforeend", `hi joe`);
-    // Object.keys(data).forEach((keys) => {
-    //   data[keys];
-    // });
+    // data.results.forEach((urls) => list.push(urls.url));
   } catch (error) {
     document.querySelector("h1").textContent = error;
     document.querySelector("h1").textContent =
@@ -29,6 +26,33 @@ async function getData(URL) {
 }
 getData(URL);
 
+console.log(list);
+
+const y = Math.floor(Math.random() * list.length);
+console.log(y);
+const URL2 = list[y - 1];
+console.log(URL2);
+
+async function getPokemon(URL2) {
+  try {
+    const response = await fetch(URL2);
+    console.log(response);
+    // 200-299
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    // take response from serve and convert it to JSON
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    document.querySelector("h1").textContent = error;
+    document.querySelector("h1").textContent =
+      "Please search for something else.";
+  }
+}
+getPokemon(URL2);
+// data.results.forEach((urls) => list.push(urls.url));
+// console.log(list);
 // function popup() {
 //   window.open("open.html", "opening", "popup");
 // }
