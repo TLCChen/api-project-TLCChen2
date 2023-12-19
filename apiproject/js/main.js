2e3110288a67e37da994f66c2d694824112009e0
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-12-19 13:15:02 -0500

    12-19-23:sure

--- apiproject/js/main.js
+++ apiproject/js/main.js
@@ -9,56 +9,44 @@ let itemBox = [];
 let again = true;
 
 async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    console.log(response);
-    // 200-299
-    if (response.status != 200) {
-      throw new Error(response.statusText);
-    }
-    // take response from serve and convert it to JSON
-    const data = await response.json();
-    console.log(data);
-    const list = data.results.map((take) => take.url);
-    console.log(list);
-    return list;
-  } catch (error) {
-    document.querySelector("h1").textContent = error;
-    document.querySelector("h1").textContent =
-      "Please search for something else.";
-  }
+  const response = await fetch(URL);
+  console.log(response);
+
+  const data = await response.json();
+  console.log(data);
+  list2 = data.results.map((take) => take.url);
+  console.log(list2);
+  return list2;
 }
 
-async function getData2() {
-  const len = list2.length;
+async function getList() {
+  const list = getData();
+  const len = list.length;
   const y = Math.floor(Math.random() * len);
   console.log(y);
-  const num = list2[y - 1];
+  const num = list[y - 1];
   console.log(num);
 
-  try {
-    const response = await fetch(num);
-    console.log(response);
-    // 200-299
-    if (response.status != 200) {
-      throw new Error(response.statusText);
-    }
-    const data = await response.json();
-    console.log(data);
-    document.querySelector(".box").insertAdjacentHTML(
-      "beforeend",
-      `<div class="item">
-      <img src=${data.sprites.front_default} alt="This is ${data.name}">
-      </div>
-      `
-    );
-    console.log(data.name);
-    return data.name;
-  } catch (error) {
-    document.querySelector("h1").textContent = error;
-    document.querySelector("h1").textContent =
-      "Please search for something else.";
-  }
+  const response = await fetch(num);
+  console.log(response);
+  // 200-299
+  const data = await response.json();
+  console.log(data);
+  console.log(data.names);
+  const all = [data.name, data.sprites.front_default];
+  return all;
+}
+
+async function getData5() {
+  const data = getList();
+  document.querySelector(".box").insertAdjacentHTML(
+    "beforeend",
+    `<div class="item">
+    <img src=${data[1]} alt="This is ${data[0]}">
+    </div>
+    `
+  );
+  console.log(data[0]);
 }
 
 async function getData3() {
@@ -66,44 +54,10 @@ async function getData3() {
   data.forEach((push) => list2.push(push));
 }
 
-async function getData4() {
-  console.log("getData4");
+async function call(){
+  getData(URL)
 }
 
-function clear() {
-  document.querySelector(".input").value = "";
+async function getDataInfinity() {
+  // now you never nefunction
 }
-
-function submit() {
-  document.querySelector("#change").addEventListener("click", function () {
-    if ((document.querySelector(".input").value = getData2())) {
-    }
-  });
-}
-async function call() {
-  await getData3();
-  // console.log(list2);
-  if (again === true) {
-    document.querySelector(".btn").addEventListener("click", async function () {
-      // document.querySelector(".item").remove();
-      document.querySelectorAll(".item").forEach((item) => itemBox.push(item));
-      console.log(itemBox);
-      // const reverseBox = itemBox.reverse()
-      // const goAway = reverseBox.filter((item)=> reverseBox.indexOf(item) != 0)
-      // const goAway1 = reverseBox.filter((item)=> reverseBox.indexOf(item) === 0)
-      // console.log(goAway)
-      // console.log(goAway1)
-      // Does not work because it leaves the last pokemon from the previous press
-      itemBox.forEach((item) => item.remove());
-      await getData2();
-      console.log(document.querySelector(".input").value);
-      console.log(again);
-    });
-  }
-}
-
-document.querySelector("#change").addEventListener("click", function () {
-  again = false;
-  console.log("apple");
-});
-call();
